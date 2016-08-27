7b904c3b18839ebd71d8117bc39af1d3c4c449e2
PerrinWaldock <perrinwaldock@gmail.com>
2016-08-26 22:57:19 -0700

    First commit ever in my github life

---
+++ ex1.py
@@ -0,0 +1,8 @@
+print "Hello World!"
+print "Hello Again!"
+print "I like typing this"
+print "Actually, I don't"
+print #"shut up, smeagle"
+print 'yay, printing!'
+print "I'd much rather you 'not'"
+print 'I "said" do not touch this!'
